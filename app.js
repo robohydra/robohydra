79be757a0bc05f876c834aa466296749218fcfc5
Esteban Manchado Velázquez <estebanm@opera.com>
2012-03-16 12:38:54 +0000

    Use the new plugin format in app.js

--- app.js
+++ app.js
@@ -3,27 +3,53 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express              = require('express'),
+    fs                   = require('fs'),
+    Hydra                = require('./lib/hydra').Hydra,
+    summonHydraBodyParts = require('./lib/hydra').summonHydraBodyParts;
 
-function showHelpAndDie() {
+function showHelpAndDie(message) {
     console.log("SYNTAX: app.js <plugin1> <plugin2> ...");
     console.log("Each plugin is a Node package returning an object with the property 'heads'");
+
+    if (message) {
+        console.log(message);
+    }
     process.exit(1);
 }
 
-
-// Receive a list of plugins to load
-if (process.argv.length <= 2) {
-    showHelpAndDie();
+function poorMansBasename(path) {
+    return path.replace(new RegExp('/$'), '').
+        replace(new RegExp('.*/'), '');
 }
-process.argv.slice(2).forEach(function(val, index) {
-    if (val === '-h' || val === '-?' || val === '--help')
-        showHelpAndDie();
-    var plugin = require("./" + val);
-    console.log("Registering hydra heads: " +
-                plugin.heads.map(function(i) { return i.name; }).join(", "));
-    routes.registerPlugin(plugin);
+
+
+// Check parameters and load Hydra configuration
+if (process.argv.length !== 3)
+    showHelpAndDie();
+var configPath = process.argv[2];
+if (configPath === '-h' || configPath === '-?' || configPath === '--help')
+    showHelpAndDie();
+var hydraConfigString = fs.readFileSync(configPath, 'utf-8');
+console.log(configPath);
+console.log(hydraConfigString);
+var hydraConfig = JSON.parse(hydraConfigString);
+if (! hydraConfig.plugins)
+    showHelpAndDie(configPath + " doesn't seem like a valid Hydra plugin (missing 'plugins' property in the top-level object)");
+
+
+var hydra = new Hydra();
+hydraConfig.plugins.forEach(function(pluginDef) {
+    var pluginModule = require("./" + pluginDef.path);
+    var pluginConf = pluginDef.config;
+    pluginConf.path = pluginDef.path;
+    var plugin = summonHydraBodyParts(pluginModule.getBodyParts(pluginConf));
+    var pluginName = poorMansBasename(pluginDef.path);
+    console.log("Registering hydra plugin " +
+                pluginName + " with heads: " +
+                plugin.heads.map(function(i) { return pluginName + "." + i.name(); }).join(", "));
+    plugin.name = pluginName;
+    hydra.registerPlugin(plugin);
 });
 
 var app = module.exports = express.createServer(express.logger());
@@ -46,7 +72,9 @@ app.configure('production', function(){
 
 
 // Routes are all dynamic, so we only need a catch-all here
-app.all('/*', routes.index);
+app.all('/*', function(req, res) {
+    hydra.handle(req, res, function() { res.end() });
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
