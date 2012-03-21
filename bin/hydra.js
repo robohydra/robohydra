ac959405f9bd8b76bfe7eadc79f091be2ab7e3e1
Esteban Manchado Velázquez <estebanm@opera.com>
2012-03-21 15:05:38 +0000

    Fix proxied POST requests

--- bin/hydra.js
+++ bin/hydra.js
@@ -56,8 +56,6 @@ var app = module.exports = express.createServer(express.logger());
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
 });
