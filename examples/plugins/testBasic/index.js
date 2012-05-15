2397c34132d0356c9575ad606e6b7b2f5a4dafda
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2012-05-15 20:46:36 +0200

    Add some assertions to the test plugin example

--- examples/plugins/testBasic/index.js
+++ examples/plugins/testBasic/index.js
@@ -1,20 +1,38 @@
-var HydraHeadStatic = require('hydra').heads.HydraHeadStatic;
+var hydra           = require('hydra'),
+    HydraHeadStatic = hydra.heads.HydraHeadStatic,
+    HydraHead       = hydra.heads.HydraHead;
 
-exports.getBodyParts = function(config) {
+exports.getBodyParts = function(config, modules) {
+    var assert = modules.assert;
+
     return {
         name: "testBasic",
         tests: {
-            firstTest: {
+            firstTestFoo: {
                 heads: [
-                    new HydraHeadStatic({path: '/bar',
+                    new HydraHeadStatic({path: '/foo',
                                          content: "fixed content"}),
                     new HydraHeadStatic({content: "more fixed content"})
                 ]
             },
-            secondTest: {
+            secondTestBar: {
                 heads: [
-                    new HydraHeadStatic({path: '/foo',
-                                         content: "fixed content"})
+                    new HydraHead({
+                        path: '/bar',
+                        handler: function(req, res) {
+                            assert.equal(1, 1, "1 should be 1");
+                            res.write("trying assertion...");
+                            assert.equal(1, 0, "1 should be 0 (!)");
+                            res.send("fixed content");
+                        }
+                    }),
+                    new HydraHead({
+                        path: '/bar2',
+                        handler: function(req, res) {
+                            assert.equal(1, 1, "1 should be 1 (still)");
+                            res.send("always works");
+                        }
+                    })
                 ]
             }
         }
