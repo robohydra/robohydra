af64c3345c8b96793a29770ce2fc8874017f887d
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2012-06-12 00:31:13 +0200

    Don't remove not-really-caching headers

--- examples/plugins/no-caching/index.js
+++ examples/plugins/no-caching/index.js
@@ -10,10 +10,6 @@ function getBodyParts(config) {
             // Tweak client cache-related headers so ensure no
             // caching, then let the request be dispatched normally
             delete req.headers['if-modified-since'];
-            delete req.headers['if-match'];
-            delete req.headers['if-none-match'];
-            delete req.headers['if-range'];
-            delete req.headers['if-unmodified-since'];
             req.headers['cache-control'] = 'no-cache';
 
             next(req, res);
