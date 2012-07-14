930d6cda1ed25735c296857211aaf94967db9774
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2012-07-14 23:26:31 +0200

    Add simple streaming example plugin

---
+++ examples/plugins/simple-streaming/index.js
@@ -0,0 +1,44 @@
+var robohydra     = require('robohydra'),
+    RoboHydraHead = robohydra.heads.RoboHydraHead,
+    Response      = robohydra.Response;
+
+exports.getBodyParts = function(config) {
+    return {heads: [
+        new RoboHydraHead({
+            name: 'exponentialWait',
+            path: '/.*',
+            handler: function(req, res, next) {
+                var wait = 200;
+
+                var res2 = new Response().
+                    on('head', function(statusCode, headers) {
+                        res.writeHead(statusCode, headers);
+                    }).
+                    on('data', function(chunk) {
+                        wait = wait * 2;
+                        setTimeout(function() {
+                            res.write(chunk);
+                        }, wait);
+                    }).
+                    on('end', function() {
+                        wait = wait * 2;
+                        setTimeout(function() {
+                            res.end();
+                        }, wait);
+                    });
+                next(req, res2);
+            }
+        }),
+
+        new RoboHydraHead({
+            name: 'dataProducer',
+            path: '/data',
+            handler: function(req, res) {
+                for (var i = 0, len = 5; i < len; i++) {
+                    res.write(new Buffer("Some data here -> " + i + "\n"));
+                }
+                res.end();
+            }
+        })
+    ]};
+};
