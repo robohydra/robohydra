5252ea3871642d98da8b3f757a922bd0aaaecf51
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2012-09-02 19:24:29 +0200

    Add RoboHydraHeadWatchdog example

---
+++ examples/plugins/request-finder/index.js
@@ -0,0 +1,31 @@
+var heads                 = require('robohydra').heads,
+    RoboHydraHeadWatchdog = heads.RoboHydraHeadWatchdog,
+    RoboHydraHeadProxy    = heads.RoboHydraHeadProxy;
+
+exports.getBodyParts = function(config) {
+    var searchTerm = config.searchTerm || 'linux';
+
+    return {
+        heads: [
+            new RoboHydraHeadWatchdog({
+                // Note that res.body is always uncompressed, even if
+                // the original response was compressed. If you really
+                // want the original body, try "res.rawBody".
+                watcher: function(req, res) {
+                    return new RegExp(searchTerm).test(res.body);
+                /* },
+                reporter: function(req, res) {
+                    console.warn("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+                    console.warn("Response for request to " + req.url + " contained " + searchTerm);
+                    console.warn("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");*/
+                }
+            }),
+
+            new RoboHydraHeadProxy({
+                mountPath: '/',
+                proxyTo: 'http://www.opera.com',
+                setHostHeader: true
+            })
+        ]
+    };
+};
