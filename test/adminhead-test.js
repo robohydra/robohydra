1d94e0099730c785e50fccb42eb594f505dcacf7
Esteban Manchado Velázquez <estebanm@opera.com>
2013-06-12 16:50:16 +0200

    Admin REST API: Plugins

--- test/adminhead-test.js
+++ test/adminhead-test.js
@@ -12,6 +12,29 @@ var helpers          = require("./helpers"),
 
 buster.spec.expose();
 
+function registerSimplePlugin(robohydra, props) {
+    var scenarios = {};
+    props.scenarios.forEach(function(scenarioName) {
+        scenarios[scenarioName] = {heads: [
+            new RoboHydraHeadStatic({
+                name: scenarioName,
+                content: "Content for scenario " + scenarioName
+            })
+        ]};
+    });
+
+    robohydra.registerPluginObject(pluginInfoObject({
+        name: props.name,
+        heads: props.heads.map(function(headName) {
+            return new RoboHydraHeadStatic({
+                name: headName,
+                content: "Content for " + headName
+            });
+        }),
+        scenarios: scenarios
+    }));
+}
+
 describe("Admin RoboHydra UI", function() {
     "use strict";
 
@@ -57,6 +80,78 @@ function restUrl(path) {
 
 describe("REST API", function() {
     "use strict";
+
+    it("shows information for the given plugin", function(done) {
+        var robohydra = new RoboHydra();
+        var pluginName = 'plugin1', headName = 'some-head-name',
+            headName2 = 'some-other-head-name';
+        registerSimplePlugin(robohydra, {
+            name: pluginName,
+            heads: [headName, headName2],
+            scenarios: ['oneAndOnlyScenario']
+        });
+
+        var url = restUrl('/plugins/' + pluginName);
+        withResponse(robohydra, url, function(resp) {
+            expect(resp.statusCode).toEqual(200);
+            var info = JSON.parse(resp.body.toString());
+            expect(info.name).toEqual(pluginName);
+            expect(info.heads).toEqual([
+                {plugin: pluginName,
+                 name: headName,
+                 attached: true},
+                {plugin: pluginName,
+                 name: headName2,
+                 attached: true}
+            ]);
+            expect(info.scenarios).toEqual([
+                {plugin: pluginName,
+                 name: 'oneAndOnlyScenario',
+                 active: false}
+            ]);
+            done();
+        });
+    });
+
+    it("updates scenario state when a scenario starts", function(done) {
+        var robohydra = new RoboHydra();
+        var pluginName = 'plugin1', headName = 'some-head-name',
+            headName2 = 'some-other-head-name', scenarioName = 'firstScenario',
+            scenarioName2 = 'secondScenario';
+        registerSimplePlugin(robohydra, {
+            name: pluginName,
+            heads: [headName, headName2],
+            scenarios: [scenarioName, scenarioName2]
+        });
+        robohydra.startScenario(pluginName, 'firstScenario');
+
+        var url = restUrl('/plugins/' + pluginName);
+        withResponse(robohydra, url, function(resp) {
+            var initialInfo = JSON.parse(resp.body.toString());
+            expect(initialInfo.scenarios).toEqual([
+                {plugin: pluginName,
+                 name: 'firstScenario',
+                 active: true},
+                {plugin: pluginName,
+                 name: 'secondScenario',
+                 active: false}
+            ]);
+
+            robohydra.startScenario(pluginName, 'secondScenario');
+            withResponse(robohydra, url, function(resp) {
+                var updatedInfo = JSON.parse(resp.body.toString());
+                expect(updatedInfo.scenarios).toEqual([
+                    {plugin: pluginName,
+                     name: 'firstScenario',
+                     active: false},
+                    {plugin: pluginName,
+                     name: 'secondScenario',
+                     active: true}
+                ]);
+                done();
+            });
+        });
+    });
 
     it("shows information for the given head", function(done) {
         var robohydra = new RoboHydra();
