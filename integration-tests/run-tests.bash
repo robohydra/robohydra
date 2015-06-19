e2b625566459e66189d82c7d1908ec20e0f6c1cd
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2015-06-20 00:57:42 +0200

    Reduce waiting in lo-fi integration tests
    Instead of always waiting a fixed amount of 5 seconds, try to wait
    as little as possible (0.1s at a time until we can connect, maximum
    5s wait).
    
    While at it, rename the "p" function to "t".

--- integration-tests/run-tests.bash
+++ integration-tests/run-tests.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function p {
+function t {
     echo -en "* $1 - "
 }
 
@@ -8,54 +8,72 @@ function kill_rh {
     kill `ps u | grep robohydra | grep -v grep | awk '{ print $2 }'`
 }
 
+function wait_for_server {
+    max_times=50
+    count=0
+    while [ $count -lt $max_times ]; do
+        curl http://localhost:${1:-3000} &>/dev/null
+        # Return code 7 means "Could not connect"
+        if [ "$?" -ne 7 ]; then
+            break
+        fi
+
+        sleep 0.1
+        count=`expr $count + 1`
+    done
+}
+
 function check {
-    ( sleep 0.5 && curl http://localhost:${2:-3000}${1:-/foo} 2>/dev/null || echo -e "\e[31mFAIL\e[39m"; kill_rh ) &
+    path=${1:-/foo}
+    port=${2:-3000}
+    ( wait_for_server $port && curl http://localhost:$port$path 2>/dev/null || echo -e "\e[31mFAIL\e[39m"; kill_rh ) &
 }
 
 function check_connection {
-    ( sleep 0.5 && curl http://localhost:${1:-3000} &>/dev/null && echo -e "\e[32mPASS\e[39m" || echo -e "\e[31mFAIL\e[39m"; kill_rh ) &
+    port=${1:-3000}
+    ( wait_for_server $port && curl http://localhost:$port &>/dev/null && echo -e "\e[32mPASS\e[39m" || echo -e "\e[31mFAIL\e[39m"; kill_rh ) &
 }
 
 RHBIN="../bin/robohydra.js -q"
 cd `dirname $0`
 
 
-p "Empty"
+t "Empty"
 check_connection
 $RHBIN -n
 
-p "Empty (file)"
+t "Empty (file)"
 check_connection
 $RHBIN empty.conf
 
-p "Plugin loading"
+t "Plugin loading"
 check
 $RHBIN -n -P pass,fail
 
-p "Plugin load path order"
+t "Plugin load path order"
 check
 $RHBIN -n -I robohydra/priority-plugins -P precedence
 
-p "Plugin load path order, several directories"
+t "Plugin load path order, several directories"
 check
 $RHBIN -n -I robohydra/bad-priority-plugins:robohydra/priority-plugins -P precedence
 
-p "Plugin load path order (file)"
+t "Plugin load path order (file)"
 check
 $RHBIN precedence.conf
 
-p "Plugin load path order (file + command-line)"
+t "Plugin load path order (file + command-line)"
 check
 $RHBIN -I robohydra/priority-plugins bad-precedence.conf
 
-p "Setting a port (command-line)"
+t "Setting a port (command-line)"
 check /foo 3001
 $RHBIN -p 3001 -n -P pass
 
-p "Setting a port (file)"
+t "Setting a port (file)"
 check /foo 3001
 $RHBIN port.conf
 
-p "Setting a port (file + command-line)"
+t "Setting a port (file + command-line)"
 check /foo 3002
 $RHBIN -p 3002 port.conf
