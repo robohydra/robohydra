dbfb1ef9e3be8771ced7a0fb9d30291aa303aac0
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2015-06-23 01:10:53 +0200

    Bump the integration suite max wait, just in case

--- integration-tests/run-tests.bash
+++ integration-tests/run-tests.bash
@@ -9,7 +9,7 @@ function kill_rh {
 }
 
 function wait_for_server {
-    max_times=50
+    max_times=100
     count=0
     while [ $count -lt $max_times ]; do
         curl http://localhost:${1:-3000} &>/dev/null
